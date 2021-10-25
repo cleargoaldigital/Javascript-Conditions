125f1356974d281a813634aba2db5aad4580b4ff
Taofeek Ajibade <81028891+cleargoaldigital@users.noreply.github.com>
2021-10-25 17:07:10 +0100

    Javascript Conditions

--- main.js
+++ main.js
@@ -29,3 +29,27 @@ function updateAllowance() {
 }
 
 updateAllowance();
+
+// Weather Report
+
+
+const select = document.querySelector('select');
+const comment = document.querySelector('p');
+
+select.addEventListener('change', setWeather);
+
+function setWeather() {
+  const choice = select.value;
+
+  if (choice === 'sunny') {
+    comment.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+  } else if (choice === 'rainy') {
+    comment.textContent = 'Rain is falling outside; take a rain coat and an umbrella, and don\'t stay out for too long.';
+  } else if (choice === 'snowing') {
+    comment.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+  } else if (choice === 'overcast') {
+    comment.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+  } else {
+    comment.textContent = '';
+  }
+}
