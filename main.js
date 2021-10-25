93c8e1784b964793b2927df0925cb26a7e3fedd1
cleargoaldigital <cleargoal.ng@gmail.com>
2021-10-25 14:55:26 +0100

    Javascript Conditions

---
+++ main.js
@@ -0,0 +1,9 @@
+// General Rules
+
+/* if (condition) {
+    code to run if condition is true.
+} else {
+    run this code id above is not true.
+} */
+
+document.querySelector('html').style.backgroundcolor = 'blue';
