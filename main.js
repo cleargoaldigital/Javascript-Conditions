bc4338b66a0ecaa7a4a33f7df17b94c0b86526f6
Taofeek Ajibade <81028891+cleargoaldigital@users.noreply.github.com>
2021-10-25 15:19:10 +0100

    Javascript Conditions

--- main.js
+++ main.js
@@ -7,3 +7,25 @@
 } */
 
 document.querySelector('html').style.backgroundcolor = 'blue';
+
+const checkBox = document.querySelector('input');
+      const para = document.querySelector('p');
+      let shoppingDone = false;
+
+      checkBox.addEventListener('change',function() {
+        checkBox.disabled = true;
+        shoppingDone = true;
+        updateAllowance();
+      });
+
+      function updateAllowance() {
+        if(shoppingDone === true) {
+          var childsAllowance = 10;
+        } else {
+          var childsAllowance = 5;
+        }
+
+        para.textContent = 'Child has earned $' + childsAllowance + ' this week.';
+      }
+
+      updateAllowance();
